6b5ab093ce44383a3fe70ac9cb094973015ce608
nehalmahajan26 <nehalmahajan28@gmail.com>
2024-03-24 12:59:01 +0530

    feature working

---
+++ README.md
@@ -0,0 +1,22 @@
+# To do list 
+A React native application that allows users to add, check off and delete items on a to do list.
+
+## Features
+<ol>
+<li>Add a new item</li>
+<li>Strike off an item without deleting it</li>
+<li>Delete an item</li>
+<li>Filter out items on the basis of <b>Completed</b> and <b>Pending</b>
+</ol>
+
+![](/demo.png)
+
+### The catch!
+With the new to-do-list application, mr. peabody now bosses around Sherman, giving him redundant tasks all through the day. Sherman is in a fit of rage now :)
+
+### Built with
+
+[React Native](https://reactnative.dev/)<br>
+[npm](https://www.npmjs.com/) Dependency Management<br>
+
+
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+
+// importing components
+import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+  // state stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // run once when the app starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+  // use effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+  // functions and events
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  // save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if(localStorage.getItem("todos") == null){
+      localStorage.setItem("todos", JSON.stringify([]));
+    }else{
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>mr. peabody's to-do-list</h1>
+      </header>
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <ToDoList 
+        filteredTodos={filteredTodos}
+        setTodos={setTodos} 
+        todos={todos}
+      />
+    </div>
+  );
+}
+
+export default App;
